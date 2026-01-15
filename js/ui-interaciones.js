415a145311ff21c1f15460355350a12f9717e1f7
Bryant Facenda <bryantffacen@gmail.com>
2026-01-15 19:12:22 -0400

    update index html

---
+++ js/ui-interaciones.js
@@ -0,0 +1,12 @@
+/* const btn = document.getElementById("menuButton");
+const menu = document.getElementById("dropdownMenu");
+
+btn.addEventListener("click", (e) => {
+  e.stopPropagation(); // Evita que el click cierre el menú inmediatamente
+  menu.classList.toggle("hidden");
+});
+
+// Cerrar si haces click afuera
+document.addEventListener("click", () => {
+  menu.classList.add("hidden");
+}); */
